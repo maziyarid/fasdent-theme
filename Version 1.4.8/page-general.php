dea5f24c336d7ddc68352107b6f459ba52582ea8
Maziyar Moradi <49054922+maziyarid@users.noreply.github.com>
2026-08-16 20:48:00 +0330

    Add files via upload

---
+++ Version 1.4.8/page-general.php
@@ -0,0 +1,7 @@
+<?php
+/** General dentistry service page. */
+$alipasandi_service_key = 'general';
+get_header();
+require get_template_directory() . '/template-parts/service-detail.php';
+get_footer();
+
